2f3299384a50d1b858987388dbe86a3eb9a93a38
Max <max.mateo@estudiantat.upc.edu>
2025-09-19 09:55:42 +0200

    Vcpkg, Cmake and glm

---
+++ WooshEngine.cpp
@@ -0,0 +1,9 @@
+﻿#include "WooshEngine.h"
+
+int main()
+{
+	glm::mat4 rotationMatrix(1.0f);
+
+	std::cout << "Hello CMake and vcpkg." << std::endl;
+	return 0;
+}
---
+++ WooshEngine.h
@@ -0,0 +1,12 @@
+﻿// WooshEngine.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto.
+
+#pragma once
+
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// TODO: Haga referencia aquí a los encabezados adicionales que el programa requiere.
