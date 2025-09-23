11e10cd44e1f82ec514cb751c1fb65058643c10a
Guillman07 <guillem.ma.dev@gmail.com>
2025-09-23 19:53:32 +0200

    Window + Esc key to quit

--- src/WooshEngine.cpp
+++ src/WooshEngine.cpp
@@ -2,8 +2,66 @@
 
 int main()
 {
-	glm::mat4 rotationMatrix(1.0f);
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
 
-	std::cout << "Hello CMake and vcpkg." << std::endl;
-	return 0;
+    // Create window
+    SDL_Window* window = SDL_CreateWindow("WooshEngine", 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    // Initialize Open GL
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+
+    // Initialize GLAD
+    gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+
+    // Initialize ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+
+    ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 150");
+
+    // Engine main loop
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+
+            if (event.type == SDL_EVENT_QUIT || event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE){
+                running = false;
+            }
+        }
+
+        // ImGui Update
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // Open ImGui window
+        ImGui::Begin("Inspector"); // Name
+        ImGui::Text("This is the inspector."); // Content
+        ImGui::End();
+
+        // ImGui Render
+        ImGui::Render();
+        glViewport(0, 0, 800, 600);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
--- src/WooshEngine.h
+++ src/WooshEngine.h
@@ -9,4 +9,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <SDL3/SDL.h>
+#include <glad/glad.h>
+#include <imgui.h>
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_opengl3.h"
 // TODO: Haga referencia aquí a los encabezados adicionales que el programa requiere.
